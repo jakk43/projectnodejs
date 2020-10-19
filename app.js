7d9003ace80ea0d13222dcc6c475a0883caab0e5
jakk43 <jakkapong314@gmail.com>
2020-10-19 22:34:09 +0700

    add covid 19 page

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost", //or name (dns)
+  user: "root",
+  password: "",
+  database: "admin",
+});
+con.connect((err) => {
+  if (err) {
+    console.log("database connect failed...");
+  } else {
+    console.log("database connected...");
+  }
+});
+
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
+var covid_19Router = require('./routes/covid_19');
 
 var app = express();
 
@@ -26,6 +45,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/covid_19', covid_19Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
