9f5b2fbd84fd482281b05bf59e03c4ccf26b0eb9
jakk43 <jakkapong314@gmail.com>
2020-10-28 01:32:21 +0700

    do noting

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
-
+var url = require('url');
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index")
 });
+
 
 
 module.exports = router;
