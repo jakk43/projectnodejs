6e1e3d4b06f310ae17b5b4b75915f1a6ddb90589
jakk43 <jakkapong314@gmail.com>
2020-11-29 01:10:16 +0700

    loop

--- public/stylesheets/home_content.js
+++ public/stylesheets/home_content.js
@@ -3,180 +3,54 @@ $.get("nav", function (data) {
 });
 
 function place1() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000004",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name").append(place_name(data));
-		$("#introduction").append(introduction(data));
-		$(".attraction_types").append(attraction_types(data));
-		$(".destination").append(destination(data));
-		$(".district").append(district(data));
-		$("#weekday_text").append(weekday_text(data));
-		$("#weekday_text_time").append(weekday_text_time(data));
-		$("#thumbnail_url").attr("src", thumbnail_url(data));
-		$("#detail").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_1_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_1_" + (i + 1)).attr("src", web_picture_urls(data, 0));
+	var myplace = ['P03000004',"P03000006","P03000003","P03000008","P03000019","P03000020"];
+	for (let i = 0; i < myplace.length; i++) {
+		$.ajax({
+			url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/"+myplace[i]+"",
+			headers: {
+				Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
+			},
+		}).done(function (data) {
+			$(".place_name"+(i+1)).append(place_name(data));
+			$("#introduction"+(i+1)).append(introduction(data));
+			$(".attraction_types"+(i+1)).append(attraction_types(data));
+			$(".destination"+(i+1)).append(destination(data));
+			$(".district"+(i+1)).append(district(data));
+			$("#weekday_text"+(i+1)).append(weekday_text(data));
+			$("#weekday_text_time"+(i+1)).append(weekday_text_time(data));
+			$("#thumbnail_url"+(i+1)).attr("src", thumbnail_url(data));
+			$("#detail"+(i+1)).append(detail(data));
+			
+			for (let k = 0; k < 3; k++) {
+				let count =  String(Number(i+1))+String(Number(k+1))
+				if(web_picture_urls(data,count) != undefined){
+					$("#web_picture_urls_"+count).attr("src", web_picture_urls(data, k));
+				}else{
+					$("#web_picture_urls_"+count).attr("src", web_picture_urls(data, 0));
+				}
+				
 			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
+	
+			if (facilities(data) != null) {
+				$("#facilities"+(i+1)).append("| สิ่งอำนวยความสะดวก : " + facilities(data));
+			}
+		});
+		
+	}
+	
 }
 
-function place2() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000006",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name2").append(place_name(data));
-		$("#introduction2").append(introduction(data));
-		$(".attraction_types2").append(attraction_types(data));
-		$(".destination2").append(destination(data));
-		$(".district2").append(district(data));
-		$("#weekday_text2").append(weekday_text(data));
-		$("#weekday_text_time2").append(weekday_text_time(data));
-		$("#thumbnail_url2").attr("src", thumbnail_url(data));
-		$("#detail2").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_2_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_2_" + (i + 1)).attr("src", web_picture_urls(data, 0));
-			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities2").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
-}
 
-function place3() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000003",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name3").append(place_name(data));
-		$("#introduction3").append(introduction(data));
-		$(".attraction_types3").append(attraction_types(data));
-		$(".destination3").append(destination(data));
-		$(".district3").append(district(data));
-		$("#weekday_text3").append(weekday_text(data));
-		$("#weekday_text_time3").append(weekday_text_time(data));
-		$("#thumbnail_url3").attr("src", thumbnail_url(data));
-		$("#detail3").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_3_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_3_" + (i + 1)).attr("src", web_picture_urls(data, 0));
-			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities3").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
-}
+			// for (let k = 0; k < 3; k++) {
+			// 	if (web_picture_urls(data, k) != undefined) {
+			// 		$("#web_picture_urls_11").attr("src", web_picture_urls(data, k));
+			// 	} else {
+			// 		$("#web_picture_urls_12").attr("src", web_picture_urls(data, 0));
+			// 	}
+			// }
 
-function place4() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000008",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name4").append(place_name(data));
-		$("#introduction4").append(introduction(data));
-		$(".attraction_types4").append(attraction_types(data));
-		$(".destination4").append(destination(data));
-		$(".district4").append(district(data));
-		$("#weekday_text4").append(weekday_text(data));
-		$("#weekday_text_time4").append(weekday_text_time(data));
-		$("#thumbnail_url4").attr("src", thumbnail_url(data));
-		$("#detail").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_4_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_4_" + (i + 1)).attr("src", web_picture_urls(data, 0));
-			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities4").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
-}
+$.when(place1()).done(function (place1_data) {});
 
-function place5() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000020",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name5").append(place_name(data));
-		$("#introduction5").append(introduction(data));
-		$(".attraction_types5").append(attraction_types(data));
-		$(".destination5").append(destination(data));
-		$(".district5").append(district(data));
-		$("#weekday_text5").append(weekday_text(data));
-		$("#weekday_text_time5").append(weekday_text_time(data));
-		$("#thumbnail_url5").attr("src", thumbnail_url(data));
-		$("#detail5").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_5_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_5_" + (i + 1)).attr("src", web_picture_urls(data, 0));
-			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities5").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
-}
-
-function place6() {
-	$.ajax({
-		url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/P03000019",
-		headers: {
-			Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
-		},
-	}).done(function (data) {
-		$(".place_name6").append(place_name(data));
-		$("#introduction6").append(introduction(data));
-		$(".attraction_types6").append(attraction_types(data));
-		$(".destination6").append(destination(data));
-		$(".district6").append(district(data));
-		$("#weekday_text6").append(weekday_text(data));
-		$("#weekday_text_time6").append(weekday_text_time(data));
-		$("#thumbnail_url6").attr("src", thumbnail_url(data));
-		$("#detail6").append(detail(data));
-		for (let i = 0; i < 3; i++) {
-			if (web_picture_urls(data, i) != undefined) {
-				$("#web_picture_urls_6_" + (i + 1)).attr("src", web_picture_urls(data, i));
-			} else {
-				$("#web_picture_urls_6_" + (i + 1)).attr("src", web_picture_urls(data, 0));
-			}
-		}
-		if (facilities(data) != null) {
-			$("#facilities6").append("| สิ่งอำนวยความสะดวก : " + facilities(data));
-		}
-	});
-}
-$.when(place1(), place2(), place3()).done(function (place1_data, place2_data, place3_data) {});
-$.when(place4(), place5(), place6()).done(function (place4_data, place5_data, place6_data) {});
 
 function place_name(data) {
 	return data.result.place_name;
