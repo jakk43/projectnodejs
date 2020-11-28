7d46fd6ce5feaacd8d9494739673ad80c8c037d4
jakk43 <jakkapong314@gmail.com>
2020-11-29 01:15:14 +0700

    format

--- public/stylesheets/home_content.js
+++ public/stylesheets/home_content.js
@@ -3,54 +3,38 @@ $.get("nav", function (data) {
 });
 
 function place1() {
-	var myplace = ['P03000004',"P03000006","P03000003","P03000008","P03000019","P03000020"];
+	var myplace = ['P03000004', "P03000006", "P03000003", "P03000008", "P03000019", "P03000020"];
 	for (let i = 0; i < myplace.length; i++) {
 		$.ajax({
-			url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/"+myplace[i]+"",
+			url: "https://tatapi.tourismthailand.org/tatapi/v5/attraction/" + myplace[i] + "",
 			headers: {
 				Authorization: "Bearer GqQmVELC0cHh9qrGNUDrl9KkZKQCWd9s6Yg1u9oUVbTqXKdXHWkl)9bjDd3gDQcFvTHPbQfsZlv3b)pqv)taLpW=====2",
 			},
 		}).done(function (data) {
-			$(".place_name"+(i+1)).append(place_name(data));
-			$("#introduction"+(i+1)).append(introduction(data));
-			$(".attraction_types"+(i+1)).append(attraction_types(data));
-			$(".destination"+(i+1)).append(destination(data));
-			$(".district"+(i+1)).append(district(data));
-			$("#weekday_text"+(i+1)).append(weekday_text(data));
-			$("#weekday_text_time"+(i+1)).append(weekday_text_time(data));
-			$("#thumbnail_url"+(i+1)).attr("src", thumbnail_url(data));
-			$("#detail"+(i+1)).append(detail(data));
-			
+			$(".place_name" + (i + 1)).append(place_name(data));
+			$("#introduction" + (i + 1)).append(introduction(data));
+			$(".attraction_types" + (i + 1)).append(attraction_types(data));
+			$(".destination" + (i + 1)).append(destination(data));
+			$(".district" + (i + 1)).append(district(data));
+			$("#weekday_text" + (i + 1)).append(weekday_text(data));
+			$("#weekday_text_time" + (i + 1)).append(weekday_text_time(data));
+			$("#thumbnail_url" + (i + 1)).attr("src", thumbnail_url(data));
+			$("#detail" + (i + 1)).append(detail(data));
 			for (let k = 0; k < 3; k++) {
-				let count =  String(Number(i+1))+String(Number(k+1))
-				if(web_picture_urls(data,count) != undefined){
-					$("#web_picture_urls_"+count).attr("src", web_picture_urls(data, k));
-				}else{
-					$("#web_picture_urls_"+count).attr("src", web_picture_urls(data, 0));
+				let count = String(Number(i + 1)) + String(Number(k + 1))
+				if (web_picture_urls(data, count) != undefined) {
+					$("#web_picture_urls_" + count).attr("src", web_picture_urls(data, k));
+				} else {
+					$("#web_picture_urls_" + count).attr("src", web_picture_urls(data, 0));
 				}
-				
 			}
-	
 			if (facilities(data) != null) {
-				$("#facilities"+(i+1)).append("| สิ่งอำนวยความสะดวก : " + facilities(data));
+				$("#facilities" + (i + 1)).append("| สิ่งอำนวยความสะดวก : " + facilities(data));
 			}
 		});
-		
 	}
-	
 }
-
-
-			// for (let k = 0; k < 3; k++) {
-			// 	if (web_picture_urls(data, k) != undefined) {
-			// 		$("#web_picture_urls_11").attr("src", web_picture_urls(data, k));
-			// 	} else {
-			// 		$("#web_picture_urls_12").attr("src", web_picture_urls(data, 0));
-			// 	}
-			// }
-
 $.when(place1()).done(function (place1_data) {});
-
 
 function place_name(data) {
 	return data.result.place_name;
